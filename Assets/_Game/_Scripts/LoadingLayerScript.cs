c4647a04d2a10a352261bc9b0772fdbd9f1ae7e0
Nguyễn Gia Minh <minhng@horusvn.com>
2024-08-28 18:03:03 +0700

    theem setup loading scene

--- Assets/_Game/_Scripts/LoadingLayerScript.cs
+++ Assets/_Game/_Scripts/LoadingLayerScript.cs
@@ -1,11 +1,13 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace _Game._Scripts
 {
     public class LoadingLayerScript : MonoBehaviour
     {
+        [SerializeField] private Image _sliderUI;
         private void Start()
         {
             this.PostEvent(EventID.LoadSceneStart);
